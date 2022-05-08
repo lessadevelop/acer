6abb3d6ec9e70bee412498eb915dfaf92d7c98bf
lessadevelop <lessadevelop@gmail.com>
2022-05-07 23:14:32 -0300

    format1

---
+++ vi/js/let/1let/assets/js/script.js
@@ -0,0 +1,7 @@
+let idade = 5
+console.log('idade = ' + idade)
+document.getElementById('ex1').innerHTML = 'idade = ' + idade
+
+let altura = 180
+console.log('altura = ' + altura)
+document.getElementById('ex2').innerHTML = 'altura = ' + altura
