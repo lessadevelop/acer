b5c41c8a352eae0e3d250e9a39da78ef0d218b29
lessadevelop <lessadevelop@gmail.com>
2022-05-09 15:56:06 -0300

    tips1

--- vi/js/let/1let/assets/js/script.js
+++
@@ -1,7 +0,0 @@
-let idade = 5
-console.log('idade = ' + idade)
-document.getElementById('ex1').innerHTML = 'idade = ' + idade
-
-let altura = 180
-console.log('altura = ' + altura)
-document.getElementById('ex2').innerHTML = 'altura = ' + altura
